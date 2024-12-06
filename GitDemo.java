7ba41e2e93a624e5c195ae70f57f8d26def48a74
Mayoz <aayush.cs10@gmail.com>
2024-12-06 20:17:44 +0545

    version 2

--- GitDemo.java
+++ GitDemo.java
@@ -2,7 +2,7 @@ package Git_Test;
 
 public class GitDemo {
 	public static void main(String[] args) {
-		System.out.println("Hello world 1");
-		System.out.println("This is version 1");
+		System.out.println("Hello world 2");
+		System.out.println("This is version 2");
 	}
 }
